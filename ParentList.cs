3cdac14b45930512f556de9f70a4bda170459dd9
OArtem <artem.ostaschenko@printguard.ru>
2020-02-04 16:21:36 +0300

    Hierarchy to one file for Task3

--- ParentList.cs
+++ ParentList.cs
@@ -63,6 +63,15 @@ abstract class ParentList<T>
   public int get_replace_status(); // успешно; список пуст
   public int get_find_status(); // следующий найден; следующий не найден; список пуст
   public int get_get_status(); // успешно; список пуст
+
+abstract TwoWayList<T>: ParentList<T>
+
+      // команды
+    // предусловие: левее курсора есть элемент; 
+    // постусловие: курсор сдвинут на один узел влево
+      public void left(); 
+
+abstract LinkedList<T>: ParentList<T>
 */
 public class Node<T>
 {
@@ -396,4 +405,41 @@ public class ParentList<T>
     {
         return get_status;
     }
+}
+
+public class TwoWayList<T>: ParentList<T>
+{
+    private int left_status;
+
+    public const int LEFT_NIL = 0; // right() ещё не вызывалась
+    public const int LEFT_OK = 1; // последняя right() отработала нормально
+    public const int LEFT_ERR = 2; // связанный список пустой
+    public const int LEFT_EMP = 3; // слева элемент отсутствует
+
+    public TwoWayList(): base()
+    {
+        left_status = LEFT_NIL;
+    }
+
+    public void Left()
+    {
+        if(Size() == 0)
+            left_status = LEFT_ERR;
+
+        if(cursor.Prev == null)
+            left_status = LEFT_EMP;
+        
+        cursor = cursor.Prev;
+        left_status = LEFT_OK;
+    }
+
+    public int Get_left_status()
+    {
+        return left_status;
+    }
+}
+
+public class LinkedList<T>: ParentList<T>
+{
+    
 }
