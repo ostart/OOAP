00d180d6fbd86f670f9ac26948649bbb7fc24703
Artem Osta <ostart@mail.ru>
2021-03-08 09:33:58 +0300

    Task12 has been done

--- HierarchyClosing.cs
+++ HierarchyClosing.cs
@@ -1,1 +1,7 @@
 // Множественное наследование классов в C# не поддерживается
+
+public abstract class General : System.Object {}
+
+public class Any : General {}
+
+public sealed class None : Any {}
