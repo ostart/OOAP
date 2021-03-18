3f8affeb26336fcb1e2a464b80411a471faa746d
Artem Osta <ostart@mail.ru>
2021-03-18 07:21:04 +0300

    Task20 has been corrected

--- InheritanceCategory2.cs
+++ InheritanceCategory2.cs
@@ -1,9 +1,37 @@
 // 4) Наследования вариаций
 // Абстрактный класс Vehicle и его реализации: Automobile, Bike, Plane etc, переопределяющие абстрактный метод базового класса Move.
+public abstract class Vehicle
+{
+    public abstract void Drive();
+}
+
+public class Automobile : Vehicle
+{
+    public override void Drive()
+    {
+        System.Console.WriteLine("I drive on a road by 4 wheels");
+    }
+}
+
+public class Bike : Vehicle
+{
+    public override void Drive()
+    {
+        System.Console.WriteLine("I drive on a road by 2 wheels");
+    }
+}
+
 // Базовый класс SEGMENT и его метод perpendicular возвращает объект SEGMENT той же длины и повернутый на 90 градусов, его наследник DOTTED_SEGMENT также имеет метод perpendicular и возвращает объект DOTTED_SEGMENT.
 
 // 5) Наследование с конкретизацией (reification inheritance)
-// Класс TABLE, описывающий таблицы в общем виде и его насделники SEQUENTIAL_TABLE и HASH_TABLE. В свою очередь SEQUENTIAL_TABLE имеет классы наследники: ARRAYED_TABLE, LINKED_TABLE, FILE_TABLE.
+// Класс TABLE, описывающий таблицы в общем виде и его наследники SEQUENTIAL_TABLE и HASH_TABLE. В свою очередь SEQUENTIAL_TABLE имеет классы наследники: ARRAYED_TABLE, LINKED_TABLE, FILE_TABLE.
+class TABLE {}
+class SEQUENTIAL_TABLE: TABLE {}
+class HASH_TABLE: TABLE {}
 
 // 6) Структурное наследование (structure inheritance)
 // Класс INTEGER наследуется от класса NUMERIC и от интерфейса COMPARABLE. От NUMERIC он получает арифметические свойства, а от COMPARABLE сигнатуры операций сравнения.
+interface COMPARABLE
+{
+    int Compare<T>(T first, T second);
+}
