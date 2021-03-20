35a0c9d0d857fe8a7049256c4a8616cd2927732c
Artem Osta <ostart@mail.ru>
2021-03-20 07:47:19 +0300

    Task22 has been corrected

--- InheritanceCategory4.cs
+++ InheritanceCategory4.cs
@@ -1,3 +1,23 @@
 // Пример наследования вида: военнослужащий армии. Там одновременно присутствует иерархия званий, иерархия должностей, иерархия подразделений.
 // В определённых условиях нужны каждые из них и выделить первостепенную иерархию не представляется возможным.
 // Каждая иерархия реализуется с помощью полиморфизма и наследования, а сам объект военнослужащего содержит соответствующие поля для каждой из иерархий
+
+public abstract class Rank {};
+public class Soldier: Rank {};
+public class Foreman: Soldier {};
+public class Ensign: Foreman {};
+public class Lieutenant: Ensign {};
+public class Captain: Lieutenant {};
+
+public abstract class Position {};
+public class Member: Position {};
+public class Branch: Member {};
+public class Platoon: Branch {};
+public class Company: Platoon {};
+public class Colonel: Company {};
+
+public class MilitaryMan 
+{
+    public Rank Rank { get;set; }
+    public Position Position { get;set; }
+}
